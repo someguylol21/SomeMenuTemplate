f01d99406d9767d4c340f7b7721eac6523bd7b7c
someguylol_21 <102875892+someguylol21@users.noreply.github.com>
2025-07-02 15:22:19 +0200

    Delete SomeMenu directory

--- SomeMenu/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SomeTemplate
-{
-    internal class PluginInfo
-    {
-        public const string Name = "SomeTemplate";
-        public const string Credits = "Credits here";
-        public const string Version = "1.0.0";
-    }
-}
